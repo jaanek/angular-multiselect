7c02d8509df15869f666cc7c091cd3130daa5a66
Sha Alibhai <shaalibhai@unboxedtechnology.com>
2014-07-22 13:29:59 -0400

    updated main controller test spec

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,7 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should attach a list of data objects to the scope to be used by the multi select drop down', function () {
+    expect(scope.values.length).toBe(3);
   });
 });
