6a201f7f2acb8897edcb3e38808c299949850de4
Sha Alibhai <shaalibhai@unboxedtechnology.com>
2014-07-22 13:50:45 -0400

    add coverage config to karma file

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -45,14 +45,24 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: [
-      'PhantomJS'
-    ],
+    browsers: [ 'PhantomJS' ],
+
+    reporters: [ 'progress', 'coverage' ],
+
+    preprocessors: {
+      'app/scripts/**/*.js': [ 'coverage' ]
+    },
+
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage'
+    },
 
     // Which plugins to enable
     plugins: [
       'karma-phantomjs-launcher',
-      'karma-jasmine'
+      'karma-jasmine',
+      'karma-coverage'
     ],
 
     // Continuous Integration mode
