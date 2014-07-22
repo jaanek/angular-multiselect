21755430d47ddd61336bef3ade14475be2420359
Sha Alibhai <shaalibhai@unboxedtechnology.com>
2014-07-22 15:29:48 -0400

    multiselect directive test spec WIP

--- test/spec/directives/multiselect.js
+++ test/spec/directives/multiselect.js
@@ -5,16 +5,45 @@ describe('Directive: multiSelect', function () {
   // load the directive's module
   beforeEach(module('multiselectApp'));
 
+  // load templates
+  beforeEach(module('app/views/directives/multi-select.html'));
+
   var element,
-    scope;
+      scope,
+      template;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
   }));
 
-  it('should replace the multi-select tag', inject(function ($compile) {
-    element = angular.element('<multi-select></multi-select>');
+  it('should replace the multi-select tag', inject(function ($compile, $templateCache) {
+    template = $templateCache.get('app/views/directives/multi-select.html');
+
+    console.log(template);
+
+    /*scope.values = [
+      {
+        value: 'one',
+        label: 'Option 1'
+      },
+
+      {
+        value: 'two',
+        label: 'Option 2'
+      },
+
+      {
+        value: 'three',
+        label: 'Option 3'
+      }
+    ];
+
+    element = angular.element('<multi-select values="values" show-filters="true" show-other="true"></multi-select>');
     element = $compile(element)(scope);
-    expect(element.text()).toBe('');
+    scope.$digest();
+
+    console.log(element);
+
+    expect(element.text()).toBe('');*/
   }));
 });
